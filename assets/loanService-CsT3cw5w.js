ab676281d4351d23da9e20e1950d9a7b268d937b
illmedicine <illmedicine@users.noreply.github.com>
2026-02-03 18:45:59 +0000

    deploy: b198c86c37121f36397e5da863b16add78a5229b

---
+++ assets/loanService-CsT3cw5w.js
@@ -0,0 +1,1 @@
+import{c as a,h as e,f as r}from"./index-4t03Pe6N.js";async function s(o){try{const t=await a({...o,status:"pending"});try{await e(r,"sendLoanNotification")({loanId:t,...o})}catch(n){console.warn("Notification sending failed, but loan was created:",n)}return t}catch(t){throw console.error("Error submitting loan request:",t),t}}export{s as submitLoanRequest};
